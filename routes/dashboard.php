ddfb544954225ef941d2ad54f721cec23113bfd0
Daniel <danielpunk4@gmail.com>
2021-10-03 21:18:01 -0500

    mejoras

--- routes/dashboard.php
+++ routes/dashboard.php
@@ -5,12 +5,13 @@ use App\Http\Controllers\Admin\ItemController;
 use App\Http\Controllers\Admin\OrderController;
 
 Route::group(['middleware' => ['auth', 'role:admin']], function () {
-    
+
+
     Route::resource('orders', OrderController::class);
     Route::resource('clients', ClientController::class);
     Route::resource('orders.items', ItemController::class);
-    Route::get('/dashboard', function () {
-        return view('dashboard');
+    Route::get('/', function () {
+        return redirect()->route('orders.index');
     })->middleware(['auth'])->name('dashboard');
     
 
