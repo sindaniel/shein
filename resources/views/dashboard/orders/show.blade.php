68e799a47520bfa540ebf776668ba39b787202d4
Daniel <danielpunk4@gmail.com>
2021-10-03 21:37:02 -0500

    mejoras

--- resources/views/dashboard/orders/show.blade.php
+++ resources/views/dashboard/orders/show.blade.php
@@ -1,11 +1,14 @@
 @extends('layouts.dashboard')
 
 @section('header')
-<h2 class="font-semibold text-xl text-gray-800 leading-tight flex justify-between">
+<h2 class="font-semibold text-xl text-gray-800 leading-tight flex justify-between items-center">
     <span>Orden #{{ $order->id }}  </span>
     <div class='flex flex-col text-right'>
         <span>${{ number_format($order->total) }}</span>
-        <span class='text-sm text-green-600'>${{ number_format($order->profit) }}</span>
+        <div class='text-sm text-green-600 flex items-center justify-end'>
+            <svg class="w-5 h-5" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12 8c-1.657 0-3 .895-3 2s1.343 2 3 2 3 .895 3 2-1.343 2-3 2m0-8c1.11 0 2.08.402 2.599 1M12 8V7m0 1v8m0 0v1m0-1c-1.11 0-2.08-.402-2.599-1M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+            <span>${{ number_format($order->profit) }}</span>
+        </div>
     </div>
 </h2>
 @endsection
@@ -41,16 +44,22 @@
                                     <td class="py-3 px-6 text-left whitespace-nowrap">
                                         <div class="flex items-center">
                                             <div class="mr-2">
-                                                <img src="{{ $item->thumb }}" class="w-10">
+                                                <a href="{{ $item->url }}" target="_blank">
+                                                    <img src="{{ $item->thumb }}" class="w-10">
+                                                </a>
                                             </div>
                                             <span class="font-medium">{{ $item->name }}</span>
                                         </div>
                                     </td>
-                                    <td  class="py-3 px-6 flex flex-col justify-center">
-                                        <strong>${{ number_format($item->price) }}</strong>
-                                        @if ($item->attempts)
-                                        <span class='text-sm text-green-600'>${{ number_format($item->attempts) }}</span>
-                                        @endif
+                                    <td  class="py-3 px-6 ">
+                                        <div class='flex flex-col justify-center'>
+                                            <strong>${{ number_format($item->price) }}</strong>
+                                    
+                                            <span class='text-sm text-green-600'>${{ number_format($item->attempts) }}</span>
+
+                                        </div>
+                                       
+                                    
                                        
                                     </td>
                                     <td class="py-3 px-6 text-left">
