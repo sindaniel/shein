2fbd1fdcaf3d85f07ea3d71adc79656562f6ad45
Daniel <danielpunk4@gmail.com>
2021-10-11 17:46:31 -0500

    mejora

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+
+
+Route::get('/log', function (Request $request) {
+    logger($request->all());
+});
